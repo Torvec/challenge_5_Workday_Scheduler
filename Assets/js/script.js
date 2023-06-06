e62b225047ce21ef3af2a6fb1638c79a8b3ca0b1
Edward Von Schondorf <edvonschondorf@gmail.com>
2023-06-05 23:06:17 -0700

    Add save and clear functionality for user entries in each hour block

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,22 +1,7 @@
-// TODO LIST: GIVEN I am using a daily planner to create a schedule
-    // WHEN I click into a time block
-      // THEN I can enter an event
-    // WHEN I click the save button for that time block
-      // THEN the text for that event is saved in local storage
-    // WHEN I refresh the page
-      // THEN the saved events persist
-
-// TODO: Add a listener for click events on the save button.
-  // This code should use the id in the containing time-block as a key to save the user input in local storage. 
-  // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-
-// TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements.
-  // HINT: How can the id attribute of each time-block be used to do this?
-
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
   
-  // Access the advanced format plugin
+  // Access the advanced format plugin so I can have 'th', 'nd', 'st' after the day number
   dayjs.extend(window.dayjs_plugin_advancedFormat);
   // Display the current day/date in this format: Monday, June 5th, 2023 and append it to currentDay ID
   var today = dayjs().format('dddd, MMMM Do, YYYY');
@@ -33,37 +18,67 @@ $(function () {
     workHours.push(hourObj);
   }
 
-  var relativeTime = '';// For past, present, or future class
+  var relativeTime;// For past, present, or future css class
   var currentTime = dayjs();// To compare the current time DayJS object with the workHours array of DayJS objects
 
   for (var i = 0; i < workHours.length; i++) {
 
-    if (currentTime.isBefore(workHours[i])) {
-      relativeTime = 'future'
+    // Determine if the time is past, present, or future
+    if (workHours[i].isBefore(currentTime)) {
+      relativeTime = 'past'
     } 
-    if (currentTime.isAfter(workHours[i])) {
-      relativeTime = 'past';
+    if (workHours[i].isAfter(currentTime)) {
+      relativeTime = 'future';
     } 
-    if (currentTime.isSame(workHours[i])) {
+    if (workHours[i].isSame(currentTime)) {
       relativeTime = 'present';
     }
 
+    // Create variables for the hour and hour text
     var hour = workHours[i].format('h');
-    var hourText = workHours[i].format('hA')
+    var hourText = workHours[i].format('h A')
+
+    // Get the saved text from local storage so it appears if the page is refreshed
+    var getSavedText = localStorage.getItem('hour-' + hour);
 
     // Create the necessary elements dynamically
     var divHoursId = $('<div>').attr('id', 'hour-' + hour).addClass('row time-block ' + relativeTime);
     var divHourClass = $('<div>').addClass('col-2 col-md-1 hour text-center').text(hourText);
-    var textAreaDesc = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3');
+    var textAreaDesc = $('<textarea>').addClass('col-8 col-md-10 description').attr('rows', '3').text(getSavedText);
     var saveButton = $('<button>').addClass('btn saveBtn col-2 col-md-1').attr('aria-label', 'save');
     var iElement = $('<i>').addClass('fas fa-save').attr('aria-hidden', 'true');
-
+    
     // Slap those elements on the page
     $(divHoursId).append(divHourClass);
     $(divHoursId).append(textAreaDesc);
     $(divHoursId).append(saveButton);
     $(saveButton).append(iElement);
     $(container).append(divHoursId);
+    
+    // Event listener for the save button
+    $(container).on('click', '.saveBtn', function () {
+      var hourId = $(this).parent().attr('id');
+      var textAreaValue = $(this).siblings('textarea').val();
+      localStorage.setItem(hourId, textAreaValue);
+    });    
   }
+
+  // Create the clear button
+  var clearButtonDiv = $('<div>').addClass('row');
+  var clearButton = $('<button>').addClass('btn clearBtn col-4 col-md-1 mx-auto mt-3').attr('aria-label', 'clear').text('Clear Schedule');
+  $(clearButtonDiv).append(clearButton);
+  $(container).append(clearButtonDiv);
+
+  // Event listener for the clear button
+  $(container).on('click', '.clearBtn', function () {
+    // Clear the local storage for each hour block
+    for (var i = 0; i < workHours.length; i++) {
+      var hourBlock = '#hour -' + workHours[i].format('h');
+      localStorage.clear(hourBlock);
+    }
+    // Clear the text area
+    $('.description').val('');
+    location.reload();
+  });
 
 });
